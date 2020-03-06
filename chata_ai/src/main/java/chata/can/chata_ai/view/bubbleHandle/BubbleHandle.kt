e77c248f50a7b411b0731608d341790e84bbea90
Carlos Buruel Ortiz <carlos@rinro.com.mx>
2020-03-06 13:40:30 -0600

    (07) Set shadow: RelativeLayout and CircleImageView programmatically

--- chata_ai/src/main/java/chata/can/chata_ai/view/bubbleHandle/BubbleHandle.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/view/bubbleHandle/BubbleHandle.kt
@@ -3,8 +3,10 @@ package chata.can.chata_ai.view.bubbleHandle
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.core.view.ViewCompat
+import androidx.core.view.setMargins
 import chata.can.chata_ai.R
 import chata.can.chata_ai.pojo.BubbleData.heightDefault
 import chata.can.chata_ai.pojo.BubbleData.marginBottomDefault
@@ -55,6 +57,38 @@ class BubbleHandle(private val context: Context)
 
 	private fun initBubbleLayout()
 	{
+		bubbleLayout = BubbleLayout(context)
+		with(bubbleLayout)
+		{
+			clipChildren = false
+			clipToPadding = false
+
+			val rl = RelativeLayout(context)
+			val lp = RelativeLayout.LayoutParams(-2, -2)
+			rl.layoutParams = lp
+			lp.setMargins(marginRightDefault,marginRightDefault,marginRightDefault,marginRightDefault)
+			rl.setBackgroundResource(R.drawable.fake_shadow)
+
+			circleImageView = CircleImageView(context)
+			with(circleImageView)
+			{
+				rl.addView(this)
+				layoutParams.height = heightDefault
+				layoutParams.width = widthDefault
+				if (layoutParams is ViewGroup.MarginLayoutParams)
+				{
+					(layoutParams as ViewGroup.MarginLayoutParams).setMargins(8, 8, 8, 8)
+				}
+				setImageResource(R.drawable.ic_bubble)
+				setCircleBackgroundColorResource(R.color.white)
+			}
+
+			addView(rl)
+
+			setShouldStickToWall(true)
+
+			bubblesManager.addBubble(bubbleLayout, 0,0)
+		}
 		/*bubbleLayout = BubbleLayout(context)
 		with(bubbleLayout)
 		{
@@ -66,11 +100,11 @@ class BubbleHandle(private val context: Context)
 			bubblesManager.addBubble(bubbleLayout, 0,0)
 		}*/
 
-		bubbleLayout = LayoutInflater.from(context)
+		/*bubbleLayout = LayoutInflater.from(context)
 			.inflate(R.layout.view_bubble_layout, null) as BubbleLayout
 		bubbleLayout.setOnBubbleClickListener { Toast.makeText(context, "Clicked!", Toast.LENGTH_SHORT).show() }
 		bubbleLayout.setShouldStickToWall(true)
-		bubblesManager.addBubble(bubbleLayout, 0,0)
+		bubblesManager.addBubble(bubbleLayout, 0,0)*/
 	}
 
 	private fun initCircleImageView()
