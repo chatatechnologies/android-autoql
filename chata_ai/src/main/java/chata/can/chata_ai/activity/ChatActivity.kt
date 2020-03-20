b0e3a8bd52662fae329ca912c03a7dc7cb220f17
Carlos Buruel Ortiz <carlos@rinro.com.mx>
2020-03-20 12:29:33 -0600

    (06) Integrate Chata colors; set Toolbar and menu in ChatActivity

--- chata_ai/src/main/java/chata/can/chata_ai/activity/ChatActivity.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/activity/ChatActivity.kt
@@ -1,20 +1,43 @@
 package chata.can.chata_ai.activity
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import chata.can.chata_ai.R
 
 class ChatActivity: AppCompatActivity()
 {
+	private lateinit var toolbar: Toolbar
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.chat_activity)
+
+		initViews()
+		initData()
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean
+	{
+		menuInflater.inflate(R.menu.chat_menu, menu)
+		return true
 	}
 
 	override fun finish()
 	{
 		super.finish()
 		overridePendingTransition(R.anim.slide_in_down, R.anim.slide_out_top)
+	}
+
+	private fun initViews()
+	{
+		toolbar = findViewById(R.id.toolbar)
+	}
+
+	private fun initData()
+	{
+		setSupportActionBar(toolbar)
 	}
 }
