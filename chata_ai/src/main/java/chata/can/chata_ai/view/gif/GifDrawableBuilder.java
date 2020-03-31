0fac4b0271048444e83ce71575f5978dc0858fc5
Carlos Buruel Ortiz <carlos@rinro.com.mx>
2020-03-31 07:45:03 -0600

    (02) integrate GifView for GIF resources

--- chata_ai/src/main/java/chata/can/chata_ai/view/gif/GifDrawableBuilder.java
+++
@@ -1,9 +0,0 @@
-package chata.can.chata_ai.view.gif;
-
-public class GifDrawableBuilder extends  GifDrawableInit<GifDrawableBuilder>{
-
-	@Override
-	protected GifDrawableBuilder self() {
-		return this;
-	}
-}
