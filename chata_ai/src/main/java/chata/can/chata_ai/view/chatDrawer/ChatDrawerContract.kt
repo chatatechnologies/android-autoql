07b20db347450a7dc068b93f9a07290b426b5a79
Carlos Buruel Ortiz <carlos@rinro.com.mx>
2020-04-22 16:50:10 -0500

    (03) remove ChatDrawer

--- chata_ai/src/main/java/chata/can/chata_ai/view/chatDrawer/ChatDrawerContract.kt
+++
@@ -1,9 +0,0 @@
-package chata.can.chata_ai.view.chatDrawer
-
-import chata.can.chata_ai.view.chatDrawer.model.DrawableChatDrawer
-
-interface ChatDrawerContract
-{
-	fun setDataAutocomplete(aMatches: ArrayList<String>)
-	fun setDrawables(drawableChatDrawer: DrawableChatDrawer)
-}
--- chata_ai/src/main/java/chata/can/chata_ai/view/chatDrawer/model/DrawableChatDrawer.kt
+++
@@ -1,9 +0,0 @@
-package chata.can.chata_ai.view.chatDrawer.model
-
-import android.graphics.drawable.GradientDrawable
-
-data class DrawableChatDrawer(
-	val shapeBackground: GradientDrawable,
-	val shapeDropDown: GradientDrawable,
-	val shapeMicrophone: GradientDrawable
-)
