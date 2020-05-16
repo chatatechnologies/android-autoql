d30a5fa703a7aed37c285ca627971b1ea59352e9
Carlos Buruel <carlos@rinro.com.mx>
2020-05-15 19:48:51 -0500

    (06) Change title and icon in actionBar in PagerActivity

--- chata_ai/src/main/java/chata/can/chata_ai/activity/pager/PagerActivity.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/activity/pager/PagerActivity.kt
@@ -1,24 +1,35 @@
 package chata.can.chata_ai.activity.pager
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.viewpager.widget.ViewPager
 import chata.can.chata_ai.R
+import chata.can.chata_ai.pojo.base.BaseActivity
+import chata.can.chata_ai.pojo.base.PageSelectedListener
 import chata.can.chata_ai.view.bubbleHandle.BubbleHandle
 
-class PagerActivity: AppCompatActivity()
+class PagerActivity: BaseActivity(R.layout.pager_queries_activity)
 {
-	private lateinit var viewPager: ViewPager
+	private var tvToolbar: TextView ?= null
+	private var ivLight: ImageView ?= null
+	private var viewPager: ViewPager ?= null
 	private val numPages = 2
 
-	override fun onCreate(savedInstanceState: Bundle?)
+	val dataMessengerTile = "Data Messenger"
+	val exploreQueriesTile = "Explore Queries"
+
+	override fun onCreateView()
 	{
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.pager_queries_activity)
+		tvToolbar = findViewById(R.id.tvToolbar)
+		ivLight = findViewById(R.id.ivLight)
 		viewPager = findViewById(R.id.viewPager)
 
 		val adapter = SlidePagerAdapter(supportFragmentManager, numPages)
-		viewPager.adapter = adapter
+		viewPager?.adapter = adapter
+
+		tvToolbar?.text = dataMessengerTile
+
+		initListener()
 	}
 
 	override fun finish()
@@ -36,5 +47,23 @@ class PagerActivity: AppCompatActivity()
 //		{
 //			SinglentonDrawer.mModel.clear()
 //		}
+	}
+
+	private fun initListener()
+	{
+		viewPager?.addOnPageChangeListener(object: PageSelectedListener
+		{
+			override fun onSelected(position: Int)
+			{
+				val pData = when(position)
+				{
+					0 -> Pair(dataMessengerTile, R.drawable.ic_light)
+					1 -> Pair(exploreQueriesTile, R.drawable.ic_chat_white)
+					else -> Pair(dataMessengerTile, R.drawable.ic_light)
+				}
+				tvToolbar?.text = pData.first
+				ivLight?.setImageResource(pData.second)
+			}
+		})
 	}
 }
