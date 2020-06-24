26e21b7e62bc536a5b2dc2f4ae01bc6f0665db95
Carlos Buruel <carlos@rinro.com.mx>
2020-06-24 10:30:26 -0500

    (03) change keys in json data for full suggestion

--- chata_ai/src/main/java/chata/can/chata_ai/pojo/chat/FullSuggestionQuery.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/pojo/chat/FullSuggestionQuery.kt
@@ -7,10 +7,13 @@ class FullSuggestionQuery(json: JSONObject): SimpleQuery(json)
 	var initQuery = ""
 	val mapSuggestion = HashMap<String, ArrayList<String>>()
 	init {
-		initQuery = json.optString("query")
-		if (json.has("full_suggestion"))
+//		initQuery = json.optString("query")
+		initQuery = json.optString("text")
+//		if (json.has("full_suggestion"))
+		if (json.has("replacements"))
 		{
-			val jaSuggestion = json.getJSONArray("full_suggestion")
+//			val jaSuggestion = json.getJSONArray("full_suggestion")
+			val jaSuggestion = json.getJSONArray("replacements")
 			for (index in 0 until jaSuggestion.length())
 			{
 				val jsonItem = jaSuggestion.getJSONObject(index)
@@ -24,15 +27,16 @@ class FullSuggestionQuery(json: JSONObject): SimpleQuery(json)
 					mapSuggestion[key] = ArrayList()
 					mapSuggestion[key]?.add(suggestion)
 				}
-				else if (jsonItem.has("suggestion_list"))
+//				else if (jsonItem.has("suggestion_list"))
+				else if (jsonItem.has("suggestions"))
 				{
 					mapSuggestion[key] = ArrayList()
-					val aJsonList = jsonItem.getJSONArray("suggestion_list")
+					val aJsonList = jsonItem.getJSONArray("suggestions")
 					for (iList in 0 until aJsonList.length())
 					{
 						val oJson = aJsonList.getJSONObject(iList)
-						var suggestion = if (oJson.has("text")) oJson.getString("text") else ""
-						val extra = if (oJson.has("value_label")) oJson.getString("value_label") else ""
+						var suggestion = oJson.optString("text") ?: ""
+						val extra = oJson.optString("value_label") ?: ""
 						if (extra.isNotEmpty())
 						{
 							suggestion = "$suggestion ($extra)"
