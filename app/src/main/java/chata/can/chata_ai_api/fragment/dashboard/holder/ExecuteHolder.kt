e5662494f6f5e20e4837fb5588b111acea017288
Carlos Buruel <carlos@rinro.com.mx>
2020-08-05 11:40:41 -0500

    (08) fix background in holders for GridAdapter

--- app/src/main/java/chata/can/chata_ai_api/fragment/dashboard/holder/ExecuteHolder.kt
+++ app/src/main/java/chata/can/chata_ai_api/fragment/dashboard/holder/ExecuteHolder.kt
@@ -4,12 +4,3 @@ import android.view.View
 import chata.can.chata_ai_api.R
 
 class ExecuteHolder(itemView: View): BaseHolder(itemView)
-{
-	private val ll1 = itemView.findViewById<View>(R.id.ll1) ?: null
-
-	override fun onPaint()
-	{
-		super.onPaint()
-		ll1?.setBackgroundColor(drawerBackgroundColor)
-	}
-}
