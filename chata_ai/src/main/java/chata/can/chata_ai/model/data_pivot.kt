488edce231b5efcc82931593d79dde541191b8f6
Carlos Buruel <carlos@rinro.com.mx>
2020-08-17 19:54:25 -0500

    (10) function structure for dataPivot

---
+++ chata_ai/src/main/java/chata/can/chata_ai/model/data_pivot.kt
@@ -0,0 +1,3 @@
+package chata.can.chata_ai.model
+
+class DataPivotRow(val posX: Int, val posY: Int, val value: Double)
--- chata_ai/src/main/java/chata/can/chata_ai/model/table_functions.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/model/table_functions.kt
@@ -1,22 +1,58 @@
 package chata.can.chata_ai.model
 
 fun getDataPivotColumn(aRows: ArrayList<ArrayList<String>>)
+	: Pair<ArrayList<ArrayList<String>>, ArrayList<String>>
 {
 	val categoriesX = ArrayList<String>()
 	val categoriesY = ArrayList<String>()
-//	var totalSum: [DataPivotRow] = []
-//	var totalDate: [[Any]] = []
+	var totalSum = ArrayList<DataPivotRow>()
+	var totalDate = ArrayList<ArrayList<Any>>()
+
 	for (row in aRows)
 	{
 		val data1 = row[0]
 		val data2 = row[1]
 		val data3 = row[2].toDoubleOrNull() ?: 0.0
 
-		if (categoriesX.indexOf(data1) == -1)
+		if (data1 !in categoriesX)
 		{
 			categoriesX.add(data1)
 		}
 
+		if (data2 !in categoriesY)
+		{
+			categoriesY.add(data2)
+			totalDate.add(arrayListOf())
+		}
 
+		val locX = categoriesX.indexOf(data1)
+		val locY = categoriesY.indexOf(data2)
+		totalDate[locY].add(data3)
+		val test = DataPivotRow(locX, locY, data3)
+		totalSum.add(test)
 	}
+
+	//revert categoriesY
+	val final = ArrayList<ArrayList<String>>()
+	for ((indexX, category) in categoriesY.withIndex())
+	{
+		val finalB = arrayListOf(category)
+		for ((indexY, _) in categoriesY.withIndex())
+		{
+			val position = totalSum.indexOfFirst { it.posX == indexX && it.posY == indexY }
+			//TODO CHECK TO MONEY
+			val value = if (position == -1) "$0" else totalSum[position].value.toString()
+			finalB.add(value)
+		}
+		final.add(finalB)
+	}
+	val header = arrayListOf("")
+	val headerFree = ArrayList<String>()
+	for (cat in categoriesX)
+	{
+		header.add(cat)
+		headerFree.add(cat)
+	}
+	final.add(0, header)
+	return Pair(final, headerFree)
 }
