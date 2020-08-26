066fa2849eaa0c47cbd0b03ed6e0ae9e90b37a59
Carlos Buruel <carlos@rinro.com.mx>
2020-08-25 21:12:09 -0500

    (11) prepare data for queryBase2

--- chata_ai/src/main/java/chata/can/chata_ai/pojo/dashboard/Dashboard.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/pojo/dashboard/Dashboard.kt
@@ -23,9 +23,15 @@ data class Dashboard(
 {
 	var secondQuery = ""
 	var secondDisplayType = ""
+
 	var queryBase: QueryBase ?= null
+	//no is necessary
 	var jsonPrimary: JSONObject ?= null
-//	var queryBase2: QueryBase ?= null
+
+	var queryBase2: QueryBase ?= null
+	//no is necessary
 	var jsonSecondary: JSONObject ?= null
+
 	var isWaitingData = false
+	var isWaitingData2 = false
 }
