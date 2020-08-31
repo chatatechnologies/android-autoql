6f5b65bc39755b584ba5c9864aadbe6e558dbe5d
Carlos Buruel <carlos@rinro.com.mx>
2020-08-31 10:55:42 -0500

    (04) use ChildSuggestion class for insert view for suggestion

--- app/src/main/java/chata/can/chata_ai_api/fragment/dashboard/holder/DynamicHolder.kt
+++ app/src/main/java/chata/can/chata_ai_api/fragment/dashboard/holder/DynamicHolder.kt
@@ -1,13 +1,18 @@
 package chata.can.chata_ai_api.fragment.dashboard.holder
 
+import android.content.Context
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import chata.can.chata_ai.extension.backgroundGrayWhite
 import chata.can.chata_ai.extension.getStringResources
+import chata.can.chata_ai.extension.margin
 import chata.can.chata_ai.holder.Holder
 import chata.can.chata_ai.listener.OnItemClickListener
-import chata.can.chata_ai.pojo.chat.QueryBase
+import chata.can.chata_ai.pojo.SinglentonDashboard
 import chata.can.chata_ai.pojo.chat.TypeChatView
 import chata.can.chata_ai.pojo.color.ThemeColor
 import chata.can.chata_ai.pojo.dashboard.Dashboard
@@ -23,14 +28,15 @@ class DynamicHolder(itemView: View): BaseHolder(itemView)
 	{
 		super.onBind(item, listener)
 		item?.let {
-			if (item is Dashboard)
+			if (it is Dashboard)
 			{
-				item.queryBase?.let { queryBase ->
+				it.queryBase?.let { queryBase ->
 					when(queryBase.typeView)
 					{
 						TypeChatView.SUGGESTION_VIEW ->
 						{
 							val viewSuggestion = getChildSuggestion(lls1.context)
+							ChildSuggestion(viewSuggestion, it)
 							lls1.addView(viewSuggestion)
 						}
 						else ->
@@ -44,15 +50,22 @@ class DynamicHolder(itemView: View): BaseHolder(itemView)
 	}
 
 	//region presenter suggestion
-	class SubSuggestion(
-		private val view: View,
-		private val queryBase: QueryBase
-	): Holder(view)
+	class ChildSuggestion(
+		view: View,
+		private val dashboard: Dashboard
+	)
 	{
 		private val tvContent = view.findViewById<TextView>(R.id.tvContent)?: null
-		private val llSuggestion = view.findViewById<LinearLayout>(R.id.tvContent)?: null
+		private val llSuggestion = view.findViewById<LinearLayout>(R.id.llSuggestion)?: null
 
-		override fun onPaint()
+		init {
+			onPaint()
+			onBind()
+		}
+
+		//Is necessary method for set data on tvTitle
+
+		private fun onPaint()
 		{
 			tvContent?.let {
 				val drawerColorPrimary =
@@ -61,9 +74,9 @@ class DynamicHolder(itemView: View): BaseHolder(itemView)
 			}
 		}
 
-		override fun onBind(item: Any?, listener: OnItemClickListener?)
+		private fun onBind()
 		{
-			queryBase.let {
+			dashboard.queryBase?.let {
 				tvContent?.context?.let { context ->
 					val introMessageRes = context.getStringResources(R.string.msg_suggestion)
 					val message = String.format(introMessageRes, it.message)
@@ -71,14 +84,47 @@ class DynamicHolder(itemView: View): BaseHolder(itemView)
 				}
 
 				val rows = it.aRows
-				llSuggestion?.removeAllViews()
-				for (index in 0 until rows.size)
-				{
-					val singleRow = rows[index]
-					singleRow.firstOrNull()?.let { suggestion ->
-						//add new view for suggestion
-//						val tv = buildSuggestionView(llSuggestion.context, suggestion, item)
-//						llSuggestion.addView(tv)
+				llSuggestion?.let {
+					it.removeAllViews()
+					for (index in 0 until rows.size)
+					{
+						val singleRow = rows[index]
+						singleRow.firstOrNull()?.let { suggestion ->
+							//add new view for suggestion
+							val tv = buildSuggestionView(it.context, suggestion, dashboard)
+							it.addView(tv)
+						}
+					}
+				}
+
+
+			}
+		}
+
+		private fun buildSuggestionView(context: Context, content: String, dashboard: Dashboard): TextView
+		{
+			return TextView(context).apply {
+				backgroundGrayWhite()
+				layoutParams = LinearLayout.LayoutParams(-1, -2)
+				margin(5f, 5f, 5f)
+				gravity = Gravity.CENTER_HORIZONTAL
+				setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+				setPadding(15,15,15,15)
+				text = content
+				setOnClickListener {
+//				view.addChatMessage(2, content)
+					val index = SinglentonDashboard.indexDashboard(dashboard)
+					if (index != -1)
+					{
+//						presenter.notifyQueryByIndex(index)
+//						presenter.callQuery(
+//							dashboard.apply {
+//								query = content
+//								title = content
+//								isWaitingData = true
+//								queryBase = null
+//							}
+//						)
 					}
 				}
 			}
