3aac53355943c69a078cccb979a1602286b25d65
Carlos Buruel <carlos@rinro.com.mx>
2020-09-21 13:35:18 -0500

    (07) export to MigrateAutoComplete

--- chata_ai/src/main/java/chata/can/chata_ai/view/typing/MigrateAutoComplete.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/view/typing/MigrateAutoComplete.kt
@@ -1,9 +1,41 @@
 package chata.can.chata_ai.view.typing
 
 import android.content.Context
+import android.os.Handler
+import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView
 
 class MigrateAutoComplete: AppCompatAutoCompleteTextView
 {
-	constructor(context: Context) : super(context)
+	var mText: CharSequence = ""
+	var mIndex = 0
+	var mDelay = 150L
+
+	constructor(context: Context): super(context)
+
+	constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+
+	private val mHandler = Handler()
+
+	private val characterAdder = object: Runnable
+	{
+		override fun run()
+		{
+			mHandler.postDelayed(this, mDelay)
+		}
+	}
+
+	fun animateText(txt: CharSequence)
+	{
+		mText = txt
+		mIndex = 0
+		setText("")
+		mHandler.removeCallbacks(characterAdder)
+		mHandler.postDelayed(characterAdder, mDelay)
+	}
+
+	fun setCharacterDelay(m: Long)
+	{
+		mDelay = m
+	}
 }
