612bd17baa58e5526851b456fe84a7049cb21281
Carlos Buruel <carlos@rinro.com.mx>
2020-11-17 15:47:55 -0600

    update libraries

--- chata_ai/src/main/java/chata/can/chata_ai/fragment/dataMessenger/DataMessengerFragment.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/fragment/dataMessenger/DataMessengerFragment.kt
@@ -93,7 +93,7 @@ class DataMessengerFragment: BaseFragment(), ChatContract.View
 //				val queryDemo = "Average revenue by area last year"
 //			  val queryDemo = "Number of invoice per customer number ordered"
 
-				val queryDemo = "Monthly expenses by vendor last year"
+				val queryDemo = "daily price for ADBE and MKTX for the last 60 days"
 //				val queryDemo = "Show me expenses last year over 10000"
 //				val queryDemo = "Last estimates over 10000"
 			etQuery.setText(queryDemo)
