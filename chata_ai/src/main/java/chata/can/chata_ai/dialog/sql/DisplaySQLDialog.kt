34e15ca37555a8a2268f0ff55bc37b89b6bfc681
Carlos Buruel <carlos@rinro.com.mx>
2020-11-18 18:26:22 -0600

    (05) set color and listener to ivCancel

--- chata_ai/src/main/java/chata/can/chata_ai/dialog/sql/DisplaySQLDialog.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/dialog/sql/DisplaySQLDialog.kt
@@ -6,35 +6,73 @@ import android.widget.ImageView
 import android.widget.TextView
 import chata.can.chata_ai.R
 import chata.can.chata_ai.dialog.BaseDialog
+import chata.can.chata_ai.extension.getParsedColor
+import chata.can.chata_ai.pojo.color.ThemeColor
+import chata.can.chata_ai.pojo.tool.DrawableBuilder
 
 class DisplaySQLDialog(
 	context: Context,
 	private val query: String
-): BaseDialog(context, R.layout.dialog_display_sql)
+): BaseDialog(context, R.layout.dialog_display_sql), View.OnClickListener
 {
-	private lateinit var rlTitle: View
+	private lateinit var rlParent: View
 	private lateinit var tvTitle: TextView
 	private lateinit var ivCancel: ImageView
+	private lateinit var vBorder: View
 	private lateinit var etQuery: TextView
 
 	override fun onCreateView()
 	{
 		super.onCreateView()
-		tvTitle.setText(R.string.generated_sql)
-		etQuery.text = formatterSQL(query)
+		setData()
 	}
 
 	override fun setColors()
 	{
-
+		ivCancel.setColorFilter(
+			context.getParsedColor(R.color.chata_drawer_background_color_dark))
+		ThemeColor.currentColor.run {
+			context.run {
+				ivCancel.setColorFilter(getParsedColor(R.color.chata_drawer_background_color_dark))
+				rlParent.setBackgroundColor(getParsedColor(drawerColorSecondary))
+				tvTitle.setTextColor(getParsedColor(drawerTextColorPrimary))
+				vBorder.setBackgroundColor(getParsedColor(drawerBorderColor))
+				etQuery.setTextColor(getParsedColor(drawerTextColorPrimary))
+				etQuery.background = DrawableBuilder.setGradientDrawable(
+					getParsedColor(drawerColorSecondary),
+					1f,
+					1,
+					getParsedColor(drawerTextColorPrimary))
+			}
+		}
 	}
 
 	override fun setViews()
 	{
-		rlTitle = findViewById(R.id.rlTitle)
+		rlParent = findViewById(R.id.rlParent)
 		tvTitle = findViewById(R.id.tvTitle)
 		ivCancel = findViewById(R.id.ivCancel)
+		vBorder = findViewById(R.id.vBorder)
 		etQuery = findViewById(R.id.etQuery)
+	}
+
+	override fun onClick(view: View?)
+	{
+		view?.let {
+			when(it.id)
+			{
+				R.id.ivCancel -> dismiss()
+				else -> {}
+			}
+		}
+	}
+
+	private fun setData()
+	{
+		tvTitle.setText(R.string.generated_sql)
+		etQuery.text = formatterSQL(query)
+
+		ivCancel.setOnClickListener(this)
 	}
 
 	private val aKeywords = arrayListOf("select", "from", "where")
