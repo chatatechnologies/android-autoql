ed60452448e8f97704c3c55971bfcf4777d10fc0
home_user <carlos@rinro.com.mx>
2021-01-04 12:37:38 -0600

    (01) update local_column with Total revenue by month in 2019 query

--- chata_ai/src/main/java/chata/can/chata_ai/fragment/dataMessenger/DataMessengerFragment.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/fragment/dataMessenger/DataMessengerFragment.kt
@@ -88,7 +88,7 @@ class DataMessengerFragment: BaseFragment(), ChatContract.View
 		if (BuildConfig.DEBUG)
 		{
 //			val queryDemo = "total revenue by area 2019"
-			val queryDemo = "All invoices last year"
+			val queryDemo = "Total revenue by month in 2019"
 			etQuery.setText(queryDemo)
 		}
 
