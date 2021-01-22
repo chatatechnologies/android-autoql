8d742168233980fb093987cf160d76b29dd89c90
home_user <carlos@rinro.com.mx>
2021-01-21 18:46:35 -0600

    (05) change between graphs; bar to pie

--- chata_ai/src/main/java/chata/can/chata_ai/pojo/webView/TestingHTML.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/pojo/webView/TestingHTML.kt
@@ -10,7 +10,6 @@ object TestingHTML
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no">
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<!--<script src="d3.v6.min.js"></script>-->
 <script src="https://d3js.org/d3.v6.min.js"></script>
 
 <title></title>
@@ -41,6 +40,10 @@ object TestingHTML
       return count;
     }
 
+    //region pie
+    
+    //endregion
+
     //NO IMPORTANT FOR SCRIPT FINAL
     var isFirst = true;
     var data = [
@@ -68,14 +71,21 @@ object TestingHTML
       left: ((digitsCount(maxValue) - 1) * 10 + 30)//plus 30 for Y axis title
     },
     width = 960 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+    height = 550 - margin.top - margin.bottom,
+    radius = Math.min(width, height) / 2;
 
-    //var color = ["#355c7d", "#6c5b7b", "#c06c84", "#f67280", "#f8b195"];
-    var color = ["#26a7df"];//Main color for bars
+    var colorPie = ["#355c7d", "#6c5b7b", "#c06c84", "#f67280", "#f8b195"];
+    var colorBi = ['#26a7df'];//Main color for bars
 
-    var scaleColor = d3.scaleOrdinal().range(color);
+    var scaleColorPie = d3.scaleOrdinal().range(colorPie);
+    var scaleColorBi = d3.scaleOrdinal().range(colorBi);
 
     //region locale settings
+    function angle(d) {
+      var a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
+      return a > 90 ? a - 180 : a;
+    }
+
     var locale = d3.formatLocale({
       "decimal": ",",//","
       "thousands": ",",//" ",
@@ -92,59 +102,65 @@ object TestingHTML
       .range([height, 0]);
 
     function updateData() {
-      var dataRoot;
+      clearSvg();
       if (isFirst) {
-        dataRoot = data;
         isFirst = false;
+        setBar();
       } else {
-        dataRoot = data1;
         isFirst = true;
+        setPie();
       }
+    }
 
-      d3.select("svg").remove();
-      var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    function clearSvg() {
+      //remove svg
+      d3.select('svg').remove();
+    }
 
-      x.domain(dataRoot.map(function(d) { return d.name; }));
-      y.domain([0, d3.max(dataRoot, function(d) { return d.value; })]);
+    function setBar() {
+      var svg = d3.select('body').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      x.domain(data.map(function(d) { return d.name; }));
+      y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
       svg.selectAll()
-      .data(dataRoot)
-      .enter().append("rect")
-      .style("fill", function(d) {
-        return scaleColor(d.value);
+      .data(data)
+      .enter().append('rect')
+      .style('fill', function(d) {
+        return scaleColorBi(d.value);
       })
-      .attr("x", function(d) { return x(d.name); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
+      .attr('x', function(d) { return x(d.name); })
+      .attr('width', x.bandwidth())
+      .attr('y', function(d) { return y(d.value); })
+      .attr('height', function(d) { return height - y(d.value); })
       .on('click', function(_, item) {
-        console.log("Name: " + item.name + ", Value: " + item.value)
+        console.log('Name: ' + item.name + ', Value: ' + item.value)
       });
 
     //the X DATA for axis bar
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+      .attr('transform', 'translate(0,' + height + ')')
       .call(d3.axisBottom(x))
       //.selectAll('.tick line').attr('opacity', 0.1)
       //Remove line on domain for X axis
-      .call(g => g.select(".domain").remove())
+      .call(g => g.select('.domain').remove())
       //region set opacity for each tick item
-      .call(g => g.selectAll(".tick line")
+      .call(g => g.selectAll('.tick line')
         .attr('opacity', 0.2))
         //endregion
-      .selectAll("text")
+      .selectAll('text')
       //rotate text
-      .attr("transform", "translate(10,10)rotate(-45)")
+      .attr('transform', 'translate(10,10)rotate(-45)')
       //Set color each item on X axis
       .attr('fill', '#909090')
-      .style("text-anchor", "end");
+      .style('text-anchor', 'end');
 
     //the Y DATA for axis bar
-    svg.append("g")
+    svg.append('g')
       //Format numbers on axis
       .call(
         d3.axisLeft(y)
@@ -153,34 +169,78 @@ object TestingHTML
         //format data for each number on Y axis
         .tickFormat(x => `${'$'}{fformat(x)}`))
       //region set lines by each value for y axis
-      .call(g => g.selectAll(".tick line").clone()
-        .attr("stroke-opacity", 0.1)
-        .attr("x2", width))
+      .call(g => g.selectAll('.tick line').clone()
+        .attr('stroke-opacity', 0.1)
+        .attr('x2', width))
       //endregion
       //Remove line on domain for Y axis
-      .call(g => g.select(".domain").remove())
-      .selectAll("text")
+      .call(g => g.select('.domain').remove())
+      .selectAll('text')
       .attr('fill', '#909090');
 
     //Add X axis label:
-    svg.append("text")
-      .attr("text-anchor", "end")
-      .style("font-size", 16)
-      .attr("x", (width / 2) + margin.top)//for center
-      .attr("y", height + margin.bottom - 10)//for set on bottom with -10
+    svg.append('text')
+      .attr('text-anchor', 'end')
+      .style('font-size', 16)
+      .attr('x', (width / 2) + margin.top)//for center
+      .attr('y', height + margin.bottom - 10)//for set on bottom with -10
       .attr('fill', '#808080')
-      .text("Month");
+      .text('Month');
 
     //Y axis label:
-    svg.append("text")
-      .attr("text-anchor", "end")
-      .style("font-size", 16)
-      .attr("transform", "rotate(-90)")
-      .attr("y", -margin.left + 20)
-      .attr("x", margin.top + (-height / 2))//center Y axis title
+    svg.append('text')
+      .attr('text-anchor', 'end')
+      .style('font-size', 16)
+      .attr('transform', 'rotate(-90)')
+      .attr('y', -margin.left + 20)
+      .attr('x', margin.top + (-height / 2))//center Y axis title
       .attr('fill', '#808080')
-      .text("Total Order Amount");
+      .text('Total Order Amount');
     }
+    
+    function setPie() {
+      var svg = d3.select('body')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+      var pie = d3.pie()
+        .value(function(d) {
+          return d.value;
+        });
+
+      var g = svg.selectAll()
+        .data(pie(data))
+        .enter().append("g");
+
+      var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+      g.append('path')
+        .attr('d', arc)
+        .style('fill', function(d) {
+          return scaleColorPie(d.data.value);
+        });
+
+      var labelArc = d3.arc()
+        .innerRadius(radius - 60)
+        .outerRadius(radius - 60);
+
+      g.append("text")
+        .style('fill', 'white')
+        .attr("dy", ".35em")
+        .attr("transform", function(d) {
+          return 'translate(' + arc.centroid(d) + '), rotate('+ angle(d) +')';
+        })
+        .style("text-anchor", "middle")
+        .text(function(d) {
+          return d.data.name;
+        });
+    }
+    
     updateData();
 </script>
 </body>
