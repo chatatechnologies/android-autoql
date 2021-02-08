7b2ada730d4b85f50d3cbbb54ca681b05a623b42
home_user <carlos@rinro.com.mx>
2021-02-08 15:38:10 -0600

    (03) init variable for DataMessenger class

--- chata_ai/src/main/java/chata/can/chata_ai/data/DataMessenger.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/data/DataMessenger.kt
@@ -1,24 +1,32 @@
 package chata.can.chata_ai.data
 
-import chata.can.chata_ai.pojo.ConstantDrawer.TOP_PLACEMENT
-import chata.can.chata_ai.pojo.ConstantDrawer.BOTTOM_PLACEMENT
-import chata.can.chata_ai.pojo.ConstantDrawer.LEFT_PLACEMENT
-import chata.can.chata_ai.pojo.ConstantDrawer.RIGHT_PLACEMENT
-import chata.can.chata_ai.pojo.ConstantDrawer.NOT_PLACEMENT
+import chata.can.chata_ai.pojo.ConstantDrawer
+import chata.can.chata_ai.pojo.autoQL.AutoQLConfig
+import chata.can.chata_ai.pojo.color.ThemeConfig
+import chata.can.chata_ai.pojo.dataFormatting.DataFormatting
 import chata.can.chata_ai.view.bubbleHandle.Authentication
 
 class DataMessenger(
 	var defaultTab: String,
 	var authentication: Authentication,
-	private val _placement: String
+	var placement: Int = ConstantDrawer.RIGHT_PLACEMENT
 )
 {
-	var placement = when(_placement)
-	{
-		"top" -> TOP_PLACEMENT
-		"bottom" -> BOTTOM_PLACEMENT
-		"left" -> LEFT_PLACEMENT
-		"right" -> RIGHT_PLACEMENT
-		else -> NOT_PLACEMENT
-	}
+	var methodCanUse: () -> Unit = {}
+	var enableExploreQueriesTab = true
+	var title = "Data Messenger"
+	var userDisplayName = "there"
+	var introMessage = "Hi ${userDisplayName}! I'm here to help you access, search and analyze your data."
+	var queryQuickStartTopics = ArrayList<String>()
+	var inputPlaceholder = "Type your queries here"
+	var maskClosable = true
+	var onMaskClick: () -> Unit = {}
+	var onVisibleChange: () -> Unit = {}
+	var maxMessages = 0
+	var clearOnClose = false
+	var enableVoiceRecord = true
+	//autoQLConfig
+	var autoql = AutoQLConfig()
+	var dataFormatting = DataFormatting()
+	var themeConfig = ThemeConfig()
 }
--- chata_ai/src/main/java/chata/can/chata_ai/pojo/color/ThemeConfig.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/pojo/color/ThemeConfig.kt
@@ -1,8 +1,8 @@
 package chata.can.chata_ai.pojo.color
 
 data class ThemeConfig(
-	var theme: String,
-	var accentColor: String,
-	val fontFamily: String,
-	val chartColors: ArrayList<String> = arrayListOf("#26A7E9", "#A5CD39", "#DD6A6A", "#FFA700", "#00C1B2")
+	var theme: String = "dark",
+	var accentColor: String = "",
+	val fontFamily: String = "sans-serif",
+	val chartColors: ArrayList<String> = arrayListOf("#355C7D", "#6C5B7B", "#C06C84", "#F67280", "#F8B195")
 )
