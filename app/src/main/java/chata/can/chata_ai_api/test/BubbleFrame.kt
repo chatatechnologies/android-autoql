8a911409c0a2acb938b3d1d2c27192e465af41ea
home_user <carlos@rinro.com.mx>
2021-03-06 12:52:17 -0600

    (03) move view with touchListener

--- app/src/main/java/chata/can/chata_ai_api/test/BubbleFrame.kt
+++ app/src/main/java/chata/can/chata_ai_api/test/BubbleFrame.kt
@@ -1,49 +1,73 @@
 package chata.can.chata_ai_api.test
 
-import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.WindowManager
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
 import chata.can.chata_ai.extension.dpToPx
 import chata.can.chata_ai.extension.getParsedColor
 import chata.can.chata_ai_api.R
 
-class BubbleFrame: FrameLayout
+class BubbleFrame: FrameLayout, View.OnTouchListener
 {
-	@SuppressLint("ClickableViewAccessibility")
-	override fun onTouchEvent(event: MotionEvent?): Boolean
+	private var _xDelta = 0
+	private var _yDelta = 0
+//	private var lastTouchDown = 0L
+
+	override fun onTouch(view: View, event: MotionEvent): Boolean
 	{
-		when(event?.action)
+		val x = event.x.toInt()
+		val y = event.y.toInt()
+
+		viewChild?.run {
+			val rect = Rect()
+			getLocalVisibleRect(rect)
+			println("Left ${rect.left}")//
+			println("Top ${rect.top}")//
+//			println("Right ${rect.right}")
+//			println("Bottom ${rect.bottom}")
+		}
+
+		when(event.action)
 		{
 			MotionEvent.ACTION_DOWN ->
 			{
-
+				(view.layoutParams as? FrameLayout.LayoutParams)?.run {
+					_xDelta = x - leftMargin
+					_yDelta = y - topMargin
+				}
 			}
 			MotionEvent.ACTION_MOVE ->
 			{
-
+				(view.layoutParams as? FrameLayout.LayoutParams)?.run {
+					leftMargin = x - _xDelta
+					topMargin = y - _yDelta
+					view.layoutParams = this
+				}
 			}
 			MotionEvent.ACTION_UP ->
 			{
-
+				//Read click
 			}
 		}
-		return super.onTouchEvent(event)
+		invalidate()
+		return true
 	}
 
 	private fun init()
 	{
-		val view = TextView(context).apply {
-			layoutParams = FrameLayout.LayoutParams(dpToPx(64f),dpToPx(64f))
+		viewChild = TextView(context).apply {
+			layoutParams = LayoutParams(dpToPx(64f),dpToPx(64f)).apply {
+				x = 64f
+			}
 			setBackgroundColor(context.getParsedColor(R.color.blue_chata_circle))
+			setOnTouchListener(this@BubbleFrame)
 		}
-		addView(view)
+		viewChild?.let { addView(it) }
 	}
-
-	private var params: WindowManager.LayoutParams ?= null
 
 	constructor(context: Context): super(context) { init() }
 
@@ -51,4 +75,8 @@ class BubbleFrame: FrameLayout
 
 	constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int)
 		: super(context, attrs, defStyleAttr) { init() }
+
+	//region View
+	private var viewChild: TextView ?= null
+	//endregion
 }
