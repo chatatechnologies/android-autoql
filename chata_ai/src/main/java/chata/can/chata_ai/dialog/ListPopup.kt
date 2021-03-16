7bc34cdcbc35756d316e2a586cc0240b34c0d14e
home_user <carlos@rinro.com.mx>
2021-03-16 10:24:38 -0600

    (03) set color to icons on showPopList

--- chata_ai/src/main/java/chata/can/chata_ai/dialog/ListPopup.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/dialog/ListPopup.kt
@@ -5,10 +5,12 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.text.SpannableStringBuilder
 import android.text.style.ImageSpan
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
 import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.graphics.drawable.DrawableCompat
 import chata.can.chata_ai.R
 import chata.can.chata_ai.dialog.listPopup.DataPopup
 import chata.can.chata_ai.dialog.sql.DisplaySQLDialog
@@ -27,71 +29,42 @@ object ListPopup
 			R.style.popupMenuStyle2
 		else R.style.popupMenuStyle1
 		val wrapper = ContextThemeWrapper(view.context, theme)
-//		PopupMenu(wrapper, view).run {
-//			menuInflater.inflate(R.menu.menu_complete, menu)
-//			show()
-//		}
-		val popupMenu = PopupMenu(wrapper, view)
-		popupMenu.inflate(R.menu.menu_complete)
-		//insertMenuItemIcons
-		val menu = popupMenu.menu
-		//hasIcons(Menu menu)
-		var hasIcon = false
-		for (index in 0 until menu.size())
-		{
-			if (menu.getItem(index).icon != null)
-			{
-				//return true
-				hasIcon = true
-				break
-			}
-			//return false
-		}
-		//todo make insertMenuItemIcon function
-		if (hasIcon)
-		{
-			for (index in 0 until menu.size())
-			{
-				insertMenuItemIcon(view.context, menu.getItem(index))
-			}
-		}
 
-		popupMenu.show()
-
-//		PopupMenu(wrapper, view).run {
-//			menu?.run {
-//				if (dataPopup?.isReduce == true)
-//				{
-//					add(1, R.id.iReportProblem, 1, R.string.report_problem).setIcon(R.mipmap.ic_launcher_tmp)
-//					add(2, R.id.iDelete, 2, R.string.delete_response).setIcon(R.mipmap.ic_launcher_tmp)
-//				}
-//				add(3, R.id.iGenerateSQL, 3, R.string.view_generated_sql).setIcon(R.mipmap.ic_launcher_tmp)
-//			}
-//			setOnMenuItemClickListener { item ->
-//				dataPopup?.run {
-//					when(item.itemId)
-//					{
-//						R.id.iReportProblem ->
-//						{
-//							showListPopup(viewRoot, queryId, chatView)
-//						}
-//						R.id.iDelete ->
-//						{
-//							adapterView?.deleteQuery(adapterPosition)
-//						}
-//						R.id.iGenerateSQL ->
-//						{
-//							DisplaySQLDialog(view.context, query).show()
-//						}
-//						else -> {}
-//					}
-//				} ?: run {
-//					DisplaySQLDialog(view.context, query).show()
-//				}
-//				true
-//			}
-//			show()
-//		}
+		PopupMenu(wrapper, view).run {
+			menu?.run {
+				if (dataPopup?.isReduce == true)
+				{
+					add(1, R.id.iReportProblem, 1, R.string.report_problem).setIcon(R.drawable.ic_report)
+					add(2, R.id.iDelete, 2, R.string.delete_response).setIcon(R.drawable.ic_delete)
+				}
+				add(3, R.id.iGenerateSQL, 3, R.string.view_generated_sql).setIcon(R.drawable.ic_database)
+			}
+			insertMenuItemIcons(view.context, this)
+			setOnMenuItemClickListener { item ->
+				dataPopup?.run {
+					when(item.itemId)
+					{
+						R.id.iReportProblem ->
+						{
+							showListPopup(viewRoot, queryId, chatView)
+						}
+						R.id.iDelete ->
+						{
+							adapterView?.deleteQuery(adapterPosition)
+						}
+						R.id.iGenerateSQL ->
+						{
+							DisplaySQLDialog(view.context, query).show()
+						}
+						else -> {}
+					}
+				} ?: run {
+					DisplaySQLDialog(view.context, query).show()
+				}
+				true
+			}
+			show()
+		}
 	}
 
 	fun showListPopup(
@@ -129,11 +102,35 @@ object ListPopup
 		}
 	}
 
+	private fun insertMenuItemIcons(context: Context, popupMenu: PopupMenu)
+	{
+		val menu = popupMenu.menu
+		if (hasIcon(menu))
+		{
+			for (index in 0 until menu.size())
+				insertMenuItemIcon(context, menu.getItem(index))
+		}
+	}
+
+	private fun hasIcon(menu: Menu): Boolean
+	{
+		for (index in 0 until menu.size())
+		{
+			if (menu.getItem(index).icon != null) return true
+		}
+		return false
+	}
+
 	private fun insertMenuItemIcon(context: Context, menuItem: MenuItem)
 	{
 		var icon = menuItem.icon
-		if (icon == null) icon = ColorDrawable(Color.TRANSPARENT)
-
+		if (icon == null)
+			icon = ColorDrawable(Color.TRANSPARENT)
+		else
+		{
+			val drawableWrap = DrawableCompat.wrap(icon).mutate()
+			DrawableCompat.setTint(drawableWrap, SinglentonDrawer.currentAccent)
+		}
 		val iconSize = context.resources.getDimensionPixelSize(R.dimen.menu_item_icon_size)
 		icon.setBounds(0, 0, iconSize, iconSize)
 		val imageSpan = ImageSpan(icon)
