f7e553dc4411b83828183ca6dd4a8f70283b7ac7
Carlos Buruel <charly.ob.26@gmail.com>
2021-04-09 13:55:38 -0500

    (02) use Executor class like of example

--- app/src/main/java/chata/can/chata_ai_api/test/BubbleTest.kt
+++ app/src/main/java/chata/can/chata_ai_api/test/BubbleTest.kt
@@ -1,28 +1,17 @@
 package chata.can.chata_ai_api.test
 
-import android.os.Handler
-import android.os.Looper
+import chata.can.chata_ai.Executor
 import chata.can.chata_ai.pojo.base.BaseActivity
 import chata.can.chata_ai_api.R
-import java.util.concurrent.Executors
 
 class BubbleTest: BaseActivity(R.layout.activity_bubble)
 {
 	override fun onCreateView()
 	{
-		val executor = Executors.newSingleThreadExecutor()
-		val handler = Handler(Looper.getMainLooper())
-
-		object: Runnable {
-			override fun run()
-			{
-				//Background work here
-				handler.post(object: Runnable {
-					override fun run() {
-						//UI Thread work here
-					}
-				})
-			}
-		}
+		Executor({
+			Thread.sleep(3000)
+		}, {
+			println("Hi!")
+		}).execute()
 	}
 }
