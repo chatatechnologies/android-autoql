9069e5945f6536bc5a362132b01b5709607f6462
unknown <charly.ob.26@gmail.com>
2021-05-13 10:59:07 -0500

    (01) set design for SwitchDM

--- chata_ai/src/main/java/chata/can/chata_ai/view/SwitchDM.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/view/SwitchDM.kt
@@ -2,10 +2,11 @@ package chata.can.chata_ai.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.FrameLayout
+import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
 import chata.can.chata_ai.R
+import chata.can.chata_ai.extension.dpToPx
 import chata.can.chata_ai.extension.getParsedColor
 import chata.can.chata_ai.extension.margin
 import chata.can.chata_ai.extension.paddingAll
@@ -24,18 +25,33 @@ class SwitchDM: RelativeLayout
 	{
 		addView(
 			RelativeLayout(context).apply {
-				layoutParams = RelativeLayout.LayoutParams(-2, -2).apply {
-					paddingAll(left = 21f)
-				}
+				layoutParams = LayoutParams(-2, -2)
 				background = DrawableBuilder.setGradientDrawable(
 					context.getParsedColor(R.color.red_notification),
-					24f,
+					56f,
 					1,
 					context.getParsedColor(R.color.blue_chata_circle)
 				)
+				//region view
+				val view = View(context).apply {
+					id = R.id.toggleSwitch
+					layoutParams = LayoutParams(dpToPx(16f), dpToPx(16f)).apply {
+						addRule(CENTER_VERTICAL)
+					}
+					background = DrawableBuilder.setOvalDrawable(
+						context.getParsedColor(R.color.blue_chata_circle)
+					)
+					margin(start = 4f, end = 4f)
+				}
+				addView(view)
+				//endregion
 				//region
 				val tv = TextView(context).apply {
-					layoutParams = RelativeLayout.LayoutParams(-2, -2)
+					id = R.id.statusSwitch
+					layoutParams = LayoutParams(-2, -2).apply {
+						addRule(ALIGN_START, R.id.toggleSwitch)
+					}
+					margin(4f)
 					//setText("Q&A On")
 					paddingAll(left = 18f, right = 18f, top = 3f, bottom = 3f)
 					setTextColor(context.getParsedColor(R.color.white))
