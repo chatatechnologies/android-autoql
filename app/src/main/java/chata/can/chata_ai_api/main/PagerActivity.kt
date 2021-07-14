b42566863615b8a77812a5b8668171b5c46c16e0
unknown <charly.ob.26@gmail.com>
2021-07-14 12:53:23 -0500

    (02) set color for icon and text for TabLayout

--- app/src/main/java/chata/can/chata_ai_api/main/PagerActivity.kt
+++ app/src/main/java/chata/can/chata_ai_api/main/PagerActivity.kt
@@ -1,6 +1,7 @@
 package chata.can.chata_ai_api.main
 
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,7 +11,7 @@ import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.viewpager.widget.ViewPager
 import chata.can.chata_ai.activity.dm.DMActivity
-import chata.can.chata_ai.pojo.ConstantDrawer
+import chata.can.chata_ai.extension.getParsedColor
 import chata.can.chata_ai.pojo.ScreenData
 import chata.can.chata_ai.pojo.base.BaseActivity
 import chata.can.chata_ai.pojo.color.ThemeColor
@@ -26,6 +27,7 @@ class PagerActivity: BaseActivity(R.layout.pager_activity)
 	private lateinit var tabLayout: TabLayout
 	private lateinit var adapter: SlidePagerAdapter
 	private lateinit var floatingView: AutoQL
+	private val aDrawable = arrayListOf(R.drawable.ic_tab_data, R.drawable.ic_tab_dashboard)
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
@@ -75,14 +77,43 @@ class PagerActivity: BaseActivity(R.layout.pager_activity)
 		finishAffinity()
 	}
 
-	private val aDrawable = arrayListOf(R.drawable.ic_tab_data, R.drawable.ic_tab_dashboard)
 	private fun addTabLayoutEvent()
 	{
+		tabLayout.run {
+			setSelectedTabIndicatorColor(getParsedColor(R.color.colorButton))
+			setTabTextColors(
+				getParsedColor(R.color.black), getParsedColor(R.color.colorButton)
+			)
+		}
 		for (index in 0 until adapter.numPages)
 		{
 			tabLayout.getTabAt(index)?.setIcon(aDrawable[index])
+			if (index == 0)
+			{
+				tabLayout.getTabAt(index)?.icon?.setColorFilter(
+					getParsedColor(R.color.colorButton), PorterDuff.Mode.SRC_IN)
+			}
+			else
+			{
+				tabLayout.getTabAt(index)?.icon?.setColorFilter(
+					getParsedColor(R.color.black), PorterDuff.Mode.SRC_IN)
+			}
 		}
+		tabLayout.setOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+			override fun onTabReselected(tab: TabLayout.Tab?) {}
 
+			override fun onTabSelected(tab: TabLayout.Tab?)
+			{
+				tab?.icon?.setColorFilter(
+					getParsedColor(R.color.colorButton), PorterDuff.Mode.SRC_IN)
+			}
+
+			override fun onTabUnselected(tab: TabLayout.Tab?)
+			{
+				tab?.icon?.setColorFilter(
+					getParsedColor(R.color.black), PorterDuff.Mode.SRC_IN)
+			}
+		})
 		(tabLayout.getChildAt(0) as? LinearLayout)?.let { tabStrip ->
 			for (index in 0 until tabStrip.childCount)
 			{
