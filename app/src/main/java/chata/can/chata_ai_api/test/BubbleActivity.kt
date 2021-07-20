f57a3a5e01b56abc397197809a0b80722158b034
unknown <charly.ob.26@gmail.com>
2021-07-20 09:50:46 -0500

    (04) make logic for MaterialButtonToggleGroup

--- app/src/main/java/chata/can/chata_ai_api/test/BubbleActivity.kt
+++ app/src/main/java/chata/can/chata_ai_api/test/BubbleActivity.kt
@@ -1,32 +1,23 @@
 package chata.can.chata_ai_api.test
 
-import android.widget.LinearLayout
-import chata.can.chata_ai.extension.getParsedColor
-import chata.can.chata_ai.extension.margin
 import chata.can.chata_ai.pojo.base.BaseActivity
 import chata.can.chata_ai_api.R
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class BubbleActivity: BaseActivity(R.layout.activity_bubble)
 {
 	override fun onCreateView()
 	{
-		findViewById<LinearLayout>(R.id.llMain)?.run {
-//			addView(
-//				TextInputLayout(context).apply {
-//					//layoutParams = getLinearLayoutParams(-1, -2)
-//					hint = "Project ID"
-//					boxBackgroundColor = getParsedColor(R.color.white)
-//					boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
-//					//setBoxCornerRadii(5f, 5f, 5f, 5f)
-//					//margin(20.5f, 10.5f, 20.5f, 10.5f)
-//					//helperText = "* Required"
-//					addView(TextInputEditText(this.context).apply {
-//						//layoutParams = getLinearLayoutParams(-1, -2)
-//					})
-//				}
-//			)
+		var lastId = 0
+		findViewById<MaterialButtonToggleGroup>(R.id.toggleButton)?.run {
+			addOnButtonCheckedListener { group, checkedId, _ ->
+				group.check(checkedId)
+				if (lastId != 0)
+				{
+					group.uncheck(lastId)
+				}
+				lastId = checkedId
+			}
 		}
 	}
 }
