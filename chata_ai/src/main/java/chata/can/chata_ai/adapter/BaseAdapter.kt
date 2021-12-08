d9f4f36f2bae5cd464dd08570c487789f73942a1
carlos Buruel <carlos@rinro.com.mx>
2021-12-08 12:28:14 -0600

    (04) config childs for top & middle container

--- chata_ai/src/main/java/chata/can/chata_ai/adapter/BaseAdapter.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/adapter/BaseAdapter.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import chata.can.chata_ai.R
+import chata.can.chata_ai.extension.dpToPx
 import chata.can.chata_ai.extension.margin
 import chata.can.chata_ai.extension.marginAll
 import chata.can.chata_ai.extension.paddingAll
@@ -72,6 +74,55 @@ open class BaseAdapter(
 				gravity = Gravity.END
 				margin(end = 50f)
 				id = R.id.llMainBase
+//				region actions
+				addView(RelativeLayout(context).apply {
+					layoutParams = RelativeLayout.LayoutParams(-2, -2)
+					margin(start = 5f, end = 5f)
+//					region report button
+					addView(ImageView(context).apply {
+					layoutParams = RelativeLayout.LayoutParams(dpToPx(40f), dpToPx(40f))
+						paddingAll(4f)
+						id = R.id.ivReport
+						setImageResource(R.drawable.ic_report)
+					})
+//					endregion
+//					region delete button
+					addView(ImageView(context).apply {
+						layoutParams = RelativeLayout.LayoutParams(dpToPx(40f), dpToPx(40f)).apply {
+							addRule(RelativeLayout.END_OF, R.id.ivReport)
+						}
+						paddingAll(4f)
+						id = R.id.ivDelete
+						setImageResource(R.drawable.ic_delete)
+					})
+//					endregion
+//					region options
+					addView(ImageView(context).apply {
+						layoutParams = RelativeLayout.LayoutParams(dpToPx(40f), dpToPx(40f)).apply {
+							addRule(RelativeLayout.END_OF, R.id.ivDelete)
+						}
+						paddingAll(4f)
+						id = R.id.ivPoints
+						setImageResource(R.drawable.ic_points)
+					})
+//					endregion
+					id = R.id.rlDelete
+				})
+//				endregion
+
+//				region content
+				addView(RelativeLayout(context).apply {
+					layoutParams = RelativeLayout.LayoutParams(-2, -2)
+					paddingAll(left = 4f, right = 4f)
+					addView(TextView(context).apply {
+						layoutParams = RelativeLayout.LayoutParams(-2, -2)
+						margin(1f)
+						paddingAll(8f)
+						id = R.id.tvContent
+					})
+					id = R.id.rvContent
+				})
+//				endregion
 			})
 			//endregion
 		}
