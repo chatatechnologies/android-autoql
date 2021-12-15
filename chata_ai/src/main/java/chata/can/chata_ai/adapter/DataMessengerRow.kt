72ed19449b58c21c33b281720cd65fd5a8752c71
carlos Buruel <carlos@rinro.com.mx>
2021-12-15 15:50:54 -0600

    (02) set childs view for query builder row

--- chata_ai/src/main/java/chata/can/chata_ai/adapter/DataMessengerRow.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/adapter/DataMessengerRow.kt
@@ -1,12 +1,20 @@
 package chata.can.chata_ai.adapter
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.util.TypedValue
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import chata.can.chata_ai.R
+import chata.can.chata_ai.extension.dpToPx
 import chata.can.chata_ai.extension.margin
+import chata.can.chata_ai.extension.paddingAll
 
 object DataMessengerRow
 {
@@ -36,9 +44,65 @@ object DataMessengerRow
 
 //				region options
 				addView(RelativeLayout(context).apply {
-					layoutParams = LinearLayout.LayoutParams(-2, -1)
+					layoutParams = LinearLayout.LayoutParams(-1, -2)
 
+					//region llQueries
+					addView(LinearLayout(context).apply {
+						layoutParams = RelativeLayout.LayoutParams(-1, -2)
+						margin(2f, 0f, 2f)
+						orientation = LinearLayout.HORIZONTAL
+						id = R.id.llQueries
 
+//						region ivBackExplore
+						addView(ImageView(context).apply {
+							layoutParams = LinearLayout.LayoutParams(dpToPx(20f), dpToPx(20f))
+							id = R.id.ivBackExplore
+						})
+//						endregion
+//						region view
+						addView(View(context).apply {
+							layoutParams = LinearLayout.LayoutParams(dpToPx(0.5f), -1)
+							setBackgroundColor(Color.BLACK)
+							margin(end = 5f)
+						})
+//						endregion
+
+//						region current explore
+						addView(LinearLayout(context).apply {
+							layoutParams = LinearLayout.LayoutParams(-1, -2)
+							orientation = LinearLayout.VERTICAL
+
+//							region tvCurrentExplore
+							addView(TextView(context).apply {
+								layoutParams = LinearLayout.LayoutParams(-1, -2)
+								setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+								setTypeface(typeface, Typeface.BOLD)
+								id = R.id.tvCurrentExplore
+							})
+//							endregion
+//							region rvQueries
+							addView(RecyclerView(context).apply {
+								layoutParams = LinearLayout.LayoutParams(-1, -2)
+								id = R.id.rvQueries
+								isVerticalScrollBarEnabled = false
+								isHorizontalScrollBarEnabled = false
+							})
+//							endregion
+						})
+//						endregion
+					})
+//					endregion
+
+					//region rvExplore
+					addView(RecyclerView(context).apply {
+						layoutParams = RelativeLayout.LayoutParams(-1, -2)
+						isVerticalScrollBarEnabled = false
+						isHorizontalScrollBarEnabled = false
+						margin(2f, end = 2f)
+						paddingAll(22f, right = 22f)
+						id = R.id.rvExplore
+					})
+					//endregion
 				})
 //				endregion
 
