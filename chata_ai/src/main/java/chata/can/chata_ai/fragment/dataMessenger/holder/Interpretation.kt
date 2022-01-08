aa4c8f7e94233adb41cc7626e3d77d226d610bc8
Carlos Buruel <carlos@rinro.com.mx>
2022-01-08 00:31:37 -0600

    (04) set blue underline

--- chata_ai/src/main/java/chata/can/chata_ai/fragment/dataMessenger/holder/Interpretation.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/fragment/dataMessenger/holder/Interpretation.kt
@@ -1,14 +1,18 @@
 package chata.can.chata_ai.fragment.dataMessenger.holder
 
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.UnderlineSpan
+import android.content.Context
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.TextView
+import chata.can.chata_ai.R
+import chata.can.chata_ai.extension.getParsedColor
 
 object Interpretation {
 	fun setUnderLine(text: String, tvSource: TextView)
 	{
-		val ssBuilder = SpannableStringBuilder(text)
+		val ssBuilder = SpannableString(text)
 		//region define range for underline
 		val limiter = '\''
 		val pScope = ArrayList<Pair<Int, Int>>()
@@ -17,7 +21,7 @@ object Interpretation {
 		{
 			if (text[index] == limiter)
 			{
-				pScope.add(Pair(index, index + 2))
+				pScope.add(Pair(index, index + 3))
 				index += 3
 			}
 			else
@@ -28,9 +32,24 @@ object Interpretation {
 		for (pair in pScope)
 		{
 			ssBuilder.setSpan(
-				UnderlineSpan(), pair.first, pair.second, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+				blueUnderline(tvSource.context),
+				pair.first,
+				pair.second,
+				0)
 		}
 		//endregion
 		tvSource.text = ssBuilder
+	}
+
+	private fun blueUnderline(context: Context) = object: ClickableSpan() {
+		override fun onClick(view: View) {}
+
+		override fun updateDrawState(textPaint: TextPaint)
+		{
+			textPaint.run {
+				color = context.getParsedColor(R.color.blue_chata_circle)
+				isUnderlineText = true
+			}
+		}
 	}
 }
