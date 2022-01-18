424d2a9978dbfd188ea8325fd00bd776e23a8d56
Carlos Buruel <carlos@rinro.com.mx>
2022-01-17 18:47:20 -0600

    (07) integrate buildFeatures for ActivityBinding

--- app/src/main/java/chata/can/chata_ai_api/retrofit/core/RetrofitHelper.kt
+++ app/src/main/java/chata/can/chata_ai_api/retrofit/core/RetrofitHelper.kt
@@ -1,14 +1,12 @@
 package chata.can.chata_ai_api.retrofit.core
 
-import chata.can.chata_ai.pojo.api1
-import chata.can.chata_ai.pojo.urlBase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
 	fun getRetrofit(): Retrofit {
 		return Retrofit.Builder()
-			.baseUrl("$urlBase$api1")
+			.baseUrl("https://drawsomething-59328-default-rtdb.europe-west1.firebasedatabase.app/")
 			.addConverterFactory(GsonConverterFactory.create())
 			.build()
 	}
--- app/src/main/java/chata/can/chata_ai_api/retrofit/ui/view/MainActivity.kt
+++ app/src/main/java/chata/can/chata_ai_api/retrofit/ui/view/MainActivity.kt
@@ -1,14 +1,34 @@
 package chata.can.chata_ai_api.retrofit.ui.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import chata.can.chata_ai_api.R
+import chata.can.chata_ai_api.databinding.ActivityMainBinding
+import chata.can.chata_ai_api.retrofit.ui.viewModel.QuoteViewModel
 
 class MainActivity: AppCompatActivity() {
-
+	private lateinit var binding: ActivityMainBinding
+	private val quoteViewModel: QuoteViewModel by viewModels()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+//	buildFeatures is necessary for ActivityMainBinding with value viewBinding = true
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+		quoteViewModel.onCreate()
+
+		quoteViewModel.quoteModel.observe(this, /*Observer*/ { currentQuote ->
+			binding.tvQuote.text = currentQuote.quote
+			binding.tvAuthor.text = currentQuote.author
+		})
+		quoteViewModel.isLoading.observe(this, /*Observer*/ {
+			binding.progress.isVisible = it
+		})
+
+		binding.viewContainer.setOnClickListener {  }
 	}
 }
