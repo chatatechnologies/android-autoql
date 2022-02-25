b73b7b7cc4567e272567139f1dcf915412ccb7ea
Carlos Buruel <carlos@rinro.com.mx>
2022-02-25 16:28:16 -0600

    (04) update axis & categories for stacked area

--- chata_ai/src/main/java/chata/can/chata_ai/pojo/html/StackedArea.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/pojo/html/StackedArea.kt
@@ -9,8 +9,9 @@ object StackedArea {
   const svg = d3
     .select('body').append('svg')
     .attr('width', width + marginLeft + margin.right)
-		.attr('height', height + margin.top + marginBottom)
+		.attr('height', height + margin.top + marginBottom);
 
+  var widthReduce = width - 105;
   const strokeWidth = 1.5;
   const svgBase = svg.append('g')
     .attr('transform', `translate(${'$'}{marginLeft}, ${'$'}{margin.top})`);
@@ -57,7 +58,7 @@ object StackedArea {
   const xScale = d3
     //use for to scale from start to final
     .scalePoint()
-    .range([0, width])
+    .range([0, widthReduce])
     .domain(
       stackedArea.map(function(d) { return d.category; })
     )
@@ -126,7 +127,7 @@ object StackedArea {
 	    g => g.selectAll('.tick line')
 	    .clone()
 	    .attr('stroke-opacity', 0.1)
-	    .attr('x2', width)
+	    .attr('x2', widthReduce)
     )
 	  //endregion
     //Remove line on domain for Y axis
@@ -135,14 +136,39 @@ object StackedArea {
 		.attr('transform', 'translate(0,-5)')  
 	  .attr('fill', '#909090');
 
-    //on left
-  addText(svgBase, 'end', 16, /*angle*/-90, /*X*/ -height/2 + sizeByLetter(axisMiddle.length), /*Y*/-marginLeft + 15, '#808080', '', axisMiddle + '', function () {
-    
-  });
+  //on left
+  addText(svgBase, 'end', 16, /*angle*/-90, /*X*/ -height/2 + sizeByLetter(axisMiddle.length), /*Y*/-marginLeft + 15, '#808080', '', axisMiddle);
 
   //on bottom
-  addText(svg, 'end', 16, /*angle*/0, /*X*/(width / 2) + sizeByLetter(axisY.length), /*Y*/height + marginBottom - 0, '#808080', '', getAxisY(), function () {
-    
+  addText(svgBase, 'end', 16, /*angle*/0, /*X*/(widthReduce / 2) + sizeByLetter(axisY.length), /*Y*/height + marginBottom, '#808080', axisY, getAxisY(), function () {
+    modalCategories(TypeManage.DATA, this.id);
+  });
+
+  //on menu
+  addText(svgBase, 'start', 14, 0, widthReduce, 0, '#808080', axisX, getAxisX(), function () {
+    modalCategories(TypeManage.DATA, this.id);
+  });
+	
+	var factorBack = margin.top;
+  var index = 0;
+  getCategoriesStack().forEach(item => {
+    if (!indexIgnore.includes(index)) {
+        
+      addText(svgBase, 'start', 12, 0, widthReduce + margin.right - 2, factorBack, '#808080', `id_${'$'}{index}`, item, function () {
+        var id = this.id;
+        //adminStacked(id);
+      });
+
+      addCircle(svgBase, widthReduce + margin.right - 12, factorBack - 4, 5, colorPie[indexCircle(index)], `idcircle_${'$'}{index}`, function () {
+        return `opacity: ${'$'}{opacityMarked.includes(index) ? '0.5' : '1'}`;
+      },
+      function () {
+        var id = this.id;
+        //adminStacked(id);
+      });
+      factorBack += 20;
+    }
+    index++;
   });
 }
 """
