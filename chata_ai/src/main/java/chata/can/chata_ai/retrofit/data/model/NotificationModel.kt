e7f41075f25022724b7472c1a1ddad854bf18798
Carlos Buruel <carlos@rinro.com.mx>
2022-02-28 12:29:18 -0600

    (04) set color & data for NotificationHolder part 1

--- chata_ai/src/main/java/chata/can/chata_ai/retrofit/data/model/NotificationModel.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/retrofit/data/model/NotificationModel.kt
@@ -1,6 +1,9 @@
 package chata.can.chata_ai.retrofit.data.model
 
+import android.view.View
 import com.google.gson.annotations.SerializedName
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class NotificationModel (
 	@SerializedName("created_at")
@@ -17,4 +20,35 @@ data class NotificationModel (
 	val state: String
 ) {
 	var isVisible = false
+
+	fun createdAtFormatted(): String {
+		return "\uD83D\uDCC5 ${toDate(createdAt)}"
+	}
+
+	fun bottomVisibility(): Int {
+		return if (isVisible) View.VISIBLE else View.GONE
+	}
+
+	private fun toDate(iDate: Int): String
+	{
+		return try {
+			val recordDate = Date(iDate * 1000L)
+			val currentDate = Date()
+
+			val formatHour = SimpleDateFormat("hh:mma", Locale.US)
+			val hour = formatHour.format(recordDate).lowercase(Locale.US)
+
+			when((currentDate.time - recordDate.time).toInt() / (1000 * 60 * 60 * 24))
+			{
+				0 -> "Today $hour"
+				1 -> "Yesterday $hour"
+				else ->
+				{
+					val format = SimpleDateFormat("MMMM d°, yyyy", Locale.US)
+					format.format(recordDate) + " at $hour"
+				}
+			}
+		}
+		catch (ex: Exception) { "" }
+	}
 }
--- chata_ai/src/main/java/chata/can/chata_ai/retrofit/ui/viewModel/NotificationViewModel.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/retrofit/ui/viewModel/NotificationViewModel.kt
@@ -1,12 +1,16 @@
 package chata.can.chata_ai.retrofit.ui.viewModel
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.view.View
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import chata.can.chata_ai.R
+import chata.can.chata_ai.pojo.SinglentonDrawer
 import chata.can.chata_ai.pojo.color.ThemeColor
+import chata.can.chata_ai.pojo.tool.DrawableBuilder
 import chata.can.chata_ai.retrofit.data.model.NotificationModel
 import chata.can.chata_ai.retrofit.domain.GetNotificationUseCase
 import chata.can.chata_ai.retrofit.ui.view.NotificationRecyclerAdapter
@@ -17,6 +21,18 @@ class NotificationViewModel: ViewModel() {
 	val totalItems = MutableLiveData<Int>()
 
 	private var notificationRecyclerAdapter: NotificationRecyclerAdapter? = null
+
+	private var white = 0
+	private var gray = 0
+	private var blue = 0
+
+	init {
+		ThemeColor.currentColor.run {
+			white = pDrawerBackgroundColor
+			gray = pDrawerTextColorPrimary
+			blue = SinglentonDrawer.currentAccent
+		}
+	}
 
 	var getNotificationUseCase = GetNotificationUseCase()
 
@@ -39,8 +55,29 @@ class NotificationViewModel: ViewModel() {
 		notificationRecyclerAdapter?.notifyItemRangeChanged(0, aNotification.size)
 	}
 
-	fun getTextColorPrimary(): Int {
-		return ThemeColor.currentColor.pDrawerTextColorPrimary
+	fun getTextColorPrimary() = gray
+
+	fun getDrawableParent(): GradientDrawable {
+		return DrawableBuilder.setGradientDrawable(white, 18f,0, gray)
+	}
+
+	fun getDrawableLeftView(position: Int): GradientDrawable? {
+		val aNotifications = notificationList.value
+		return aNotifications?.get(position)?.let { notification ->
+			val color = if (notification.state == "DISMISSED") Color.TRANSPARENT else blue
+			DrawableBuilder.setGradientDrawable(
+				color,
+				aCornerRadius = floatArrayOf(15f, 15f, 0f, 0f, 0f, 0f, 15f, 15f)
+			)
+		}
+	}
+
+	fun getWidthLeft(): Int {
+		return 12
+	}
+
+	fun getHeightLeft(): Int {
+		return 20
 	}
 
 	fun getNotificationAt(position: Int): NotificationModel? {
