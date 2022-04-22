797464169c8b08bda0ae5fd37f6e464cd170c8f6
Carlos Buruel <carlos@rinro.com.mx>
2022-04-22 16:20:22 -0500

    (06) center progress in screen with content behind

--- app/src/main/java/chata/can/chata_ai_api/component/StackComponent.kt
+++ app/src/main/java/chata/can/chata_ai_api/component/StackComponent.kt
@@ -1,17 +1,19 @@
 package chata.can.chata_ai_api.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,25 +21,43 @@ import chata.can.chata_ai.compose.ui.theme.ApiChataTheme
 
 @Composable
 fun StackComponent() {
-	Box(
-		modifier = Modifier
-			.fillMaxWidth()
-			.height(200.dp),
-		contentAlignment = Alignment.Center,
-	) {
-		Text(
-			text = "Title",
-			style = TextStyle(
-				fontFamily = FontFamily.Monospace,
-				fontWeight = FontWeight.W900,
-				fontSize = 14.sp
-			),
+	Scaffold(content = {
+		Box(
 			modifier = Modifier
-				.padding(16.dp)
-		)
+				.fillMaxSize(),
+			contentAlignment = Alignment.Center,
+		) {
+			Column(
+				modifier = Modifier
+					.background(Color.LightGray)
+					.fillMaxSize(),
+				verticalArrangement = Arrangement.Center,
+				horizontalAlignment = Alignment.CenterHorizontally
+			) {
+				Text(
+					text = "Content under Circular Progress Indicator",
+					style = TextStyle(
+						fontFamily = FontFamily.Monospace,
+						fontWeight = FontWeight.W900,
+						fontSize = 14.sp,
+						textAlign = TextAlign.Center,
+						background = Color.Gray
+					),
+					modifier = Modifier.padding(16.dp)
+				)
+			}
 
-		CircularProgressIndicator()
-	}
+			Surface(color = Color.White) {
+				CircularProgressIndicator(
+					color = Color.Blue,
+					modifier = Modifier
+						.size(112.dp)
+						.padding(16.dp),
+					strokeWidth = 4.dp
+				)
+			}
+		}
+	})
 }
 
 @Preview(showBackground = true)
