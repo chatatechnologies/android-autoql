fc74ba9b60cdce6319e9aa7db0c6de9170f0b12b
Carlos Buruel <carlos@rinro.com.mx>
2022-04-25 11:01:23 -0500

    (04) fix value changed on CustomTextField

--- chata_ai/src/main/java/chata/can/chata_ai/compose/widget/CustomTextField.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/compose/widget/CustomTextField.kt
@@ -1,44 +1,60 @@
 package chata.can.chata_ai.compose.widget
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CustomTextField(
 	modifier: Modifier = Modifier,
-	text: String,
+	text: String = "Text test",
 	textColor: Color,
 	placeholder: String,
 	placeholderColor: Color,
-	backgroundColor: Color = Color.Transparent,
+	backgroundColor: Color = Color.White,
 	onValueChanged: (String) -> Unit
 ) {
+	val focusManager = LocalFocusManager.current
 	TextField(
 		modifier = modifier,
 		colors = TextFieldDefaults.textFieldColors(
-			backgroundColor = backgroundColor,
-			placeholderColor = placeholderColor,
-			textColor = textColor
+			backgroundColor = backgroundColor
 		),
-		placeholder = {
-			Text(text = placeholder, style = TextStyle(color = placeholderColor))
-		},
+		keyboardActions = KeyboardActions(
+			onDone = { focusManager.clearFocus() }
+		),
 		keyboardOptions = KeyboardOptions.Default.copy(
 			imeAction = ImeAction.Done,
 			keyboardType = KeyboardType.Text
 		),
 		onValueChange = onValueChanged,
 		shape = RoundedCornerShape(0.dp),
+		textStyle = TextStyle(color = textColor),
 		value = text
 	)
+}
+
+@Preview
+@Composable
+fun CustomTextFieldPreview() {
+	CustomTextField(
+		textColor = Color.White,
+		placeholder = "",
+		placeholderColor = Color.Red,
+		backgroundColor = Color.DarkGray
+	) {
+
+	}
 }
--- chata_ai/src/main/java/chata/can/chata_ai/screens/exploreQueries/explore_query.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/screens/exploreQueries/explore_query.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import chata.can.chata_ai.R
 import chata.can.chata_ai.compose.widget.CustomTextField
-import chata.can.chata_ai.pojo.color.ThemeColor
 
 @Composable
 fun ExploreQueriesSearch(modifier: Modifier = Modifier) {
@@ -26,6 +25,7 @@ fun ExploreQueriesSearch(modifier: Modifier = Modifier) {
 //	val placeholderColor = colorResource(id = R.color.place_holder)
 	val placeholderColor = Color.Red
 	var search by remember { mutableStateOf("") }
+
 	Row(modifier = modifier.padding(8.dp)) {
 		Image(
 			modifier = Modifier.size(48.dp),
@@ -34,18 +34,31 @@ fun ExploreQueriesSearch(modifier: Modifier = Modifier) {
 			contentScale = ContentScale.Fit,
 			alignment = Alignment.Center
 		)
+//		CustomOutlinedTextField(placeholder = "placeholder", value = search1, textColor = Color.White) {
+//			search1 = it
+//		}
 		CustomTextField(
-			modifier = Modifier
-				.height(48.dp)
-				.fillMaxWidth()
-				.padding(vertical = 16.dp, horizontal = 8.dp),
 			text = search,
-			textColor = colorTextField,
-			placeholder = stringResource(id = R.string.explore_queries_hint),
-			placeholderColor = placeholderColor
+			textColor = Color.White,
+			placeholder = "",
+			placeholderColor = Color.Red,
+			backgroundColor = Color.DarkGray
 		) {
 			search = it
 		}
+
+//		CustomTextField(
+//			modifier = Modifier
+//				.height(56.dp)
+//				.fillMaxWidth()
+//				.padding(vertical = 16.dp, horizontal = 8.dp),
+//			text = search,
+//			textColor = colorTextField,
+//			placeholder = stringResource(id = R.string.explore_queries_hint),
+//			placeholderColor = placeholderColor
+//		) {
+//			search = it
+//		}
 	}
 }
 
