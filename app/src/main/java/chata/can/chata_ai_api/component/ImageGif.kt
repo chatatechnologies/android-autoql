38fcc3812939e50f187c0c66c67c4466f77ca52d
Carlos Buruel <carlos@rinro.com.mx>
2022-04-26 09:30:14 -0500

    (03) git animate with coil not animate

--- app/src/main/java/chata/can/chata_ai_api/component/ImageGif.kt
+++ app/src/main/java/chata/can/chata_ai_api/component/ImageGif.kt
@@ -6,33 +6,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import chata.can.chata_ai_api.R
-import coil.Coil
 import coil.ImageLoader
 import coil.compose.rememberImagePainter
 import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
-import coil.size.OriginalSize
 
 @Composable
 fun ImageGif() {
 	val context = LocalContext.current
 	val imageLoader = ImageLoader.Builder(context)
-		.componentRegistry {
+
+		.components {
 			if (Build.VERSION.SDK_INT >= 28) {
-				ImageDecoderDecoder(context)
+				ImageDecoderDecoder.Factory()
 			} else {
-				add(GifDecoder())
+				add(GifDecoder.Factory())
 			}
 		}
 		.build()
-	Coil.setImageLoader(imageLoader)
+
 	Image(
-		painter = rememberImagePainter(
-			imageLoader = imageLoader,
-			data = R.drawable.gif_balls,
-			builder = {
-				size(OriginalSize)
-			}),
+		painter = rememberImagePainter(data = R.drawable.gif_balls, imageLoader = imageLoader),
 		contentDescription = ""
 	)
 
