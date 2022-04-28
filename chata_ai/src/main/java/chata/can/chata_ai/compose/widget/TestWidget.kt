4e9b708dfa48861a0683f42fda856a0c58f9d45b
Carlos Buruel <carlos@rinro.com.mx>
2022-04-27 19:41:46 -0500

    (08) autocomplete part 2

--- chata_ai/src/main/java/chata/can/chata_ai/compose/widget/TestWidget.kt
+++ chata_ai/src/main/java/chata/can/chata_ai/compose/widget/TestWidget.kt
@@ -1,12 +1,14 @@
 package chata.can.chata_ai.compose.widget
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -15,11 +17,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -51,7 +56,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TopBar() {
 	TopAppBar(
-		title = { Text(text = "Country List", fontSize = 20.sp) },
+		title = {
+			Text(
+				text = "Country List",
+				fontSize = 20.sp,
+				modifier = Modifier.fillMaxWidth(),
+				textAlign = TextAlign.Center
+			)
+		},
 		backgroundColor = Purple500,
 		contentColor = Color.White
 	)
@@ -60,11 +72,13 @@ fun TopBar() {
 @Composable
 fun CountryNavigation() {
 	val navController = rememberNavController()
-	NavHost(navController = navController, startDestination = "countryList") {
-		composable("countryList") {
+	NavHost(
+		navController = navController,
+		startDestination = "CountryList"
+	) {
+		composable("CountryList") {
 			CountryListScreen(navController)
 		}
-
 	}
 }
 
@@ -82,8 +96,10 @@ fun SearchCountryList(textVal: MutableState<TextFieldValue>) {
 	TextField(
 		value = textVal.value,
 		onValueChange = { textVal.value = it },
-		modifier = Modifier.fillMaxWidth(),
-		textStyle = TextStyle(Color.White, fontSize = 18.sp),
+		placeholder = { Text(text = "Search Country Name") },
+		modifier = Modifier
+			.fillMaxWidth(),
+		textStyle = TextStyle(Color.Black, fontSize = 18.sp),
 		leadingIcon = {
 			Icon(
 				imageVector = Icons.Filled.Search,
@@ -95,9 +111,11 @@ fun SearchCountryList(textVal: MutableState<TextFieldValue>) {
 		},
 		trailingIcon = {
 			if (textVal.value != TextFieldValue("")) {
-				IconButton(onClick = {
-					textVal.value = TextFieldValue("")
-				}) {
+				IconButton(
+					onClick = {
+						textVal.value = TextFieldValue("")
+					}
+				) {
 					Icon(
 						imageVector = Icons.Filled.Close,
 						contentDescription = "Close",
@@ -111,11 +129,11 @@ fun SearchCountryList(textVal: MutableState<TextFieldValue>) {
 		singleLine = true,
 		shape = RectangleShape,
 		colors = TextFieldDefaults.textFieldColors(
-			textColor = Color.White,
-			cursorColor = Color.White,
-			leadingIconColor = Color.White,
-			trailingIconColor = Color.White,
-			backgroundColor = Color.LightGray,
+			textColor = Color.Black,
+			cursorColor = Color.Black,
+			leadingIconColor = Color.Black,
+			trailingIconColor = Color.Black,
+			backgroundColor = Color.White,
 			focusedIndicatorColor = Color.Transparent,
 			unfocusedIndicatorColor = Color.Transparent,
 			disabledIndicatorColor = Color.Transparent
@@ -125,7 +143,60 @@ fun SearchCountryList(textVal: MutableState<TextFieldValue>) {
 
 @Composable
 fun CountryList(textVal: MutableState<TextFieldValue>) {
+	val context = LocalContext.current
 	val countries = getListOfCountries()
+	var filteredCountries: ArrayList<String>
+	LazyColumn(
+		modifier = Modifier.fillMaxWidth()
+	) {
+		val searchText = textVal.value.text
+		filteredCountries = if (searchText.isEmpty()) {
+			countries
+		} else {
+			val resultList = ArrayList<String>()
+			for (country in countries) {
+				if (country.lowercase(Locale.getDefault())
+						.contains(searchText.lowercase(Locale.getDefault()))
+				) {
+					resultList.add(country)
+				}
+			}
+			resultList
+		}
+		items(filteredCountries) { filteredCountries ->
+			CountryListItem(
+				countryText = filteredCountries,
+				onItemClick = { selectedCountry ->
+					Toast.makeText(context, selectedCountry, Toast.LENGTH_SHORT).show()
+				}
+			)
+		}
+	}
+}
+
+@Composable
+fun CountryListItem(
+	countryText: String,
+	onItemClick: (String) -> Unit
+) {
+	Row(
+		modifier = Modifier
+			.clickable {
+				onItemClick(countryText)
+			}
+			.background(Color.White)
+			.height(60.dp)
+			.fillMaxWidth()
+			.padding(5.dp),
+		verticalAlignment = Alignment.CenterVertically
+	) {
+		Text(
+			text = countryText,
+			fontSize = 16.sp,
+			color = Color.Black,
+			modifier = Modifier.padding(start = 10.dp)
+		)
+	}
 }
 
 @Composable
@@ -135,12 +206,12 @@ fun getListOfCountries(): ArrayList<String> {
 	for (countryCode in isoCountryCodes) {
 		val locale = Locale("", countryCode)
 		val countryName = locale.displayCountry
-		val flagOffset = 0x1FF1E6
+		val flagOffset = 0x1F1E6
 		val asciiOffset = 0x41
 		val firstChar = Character.codePointAt(countryCode, 0) - asciiOffset + flagOffset
 		val secondChar = Character.codePointAt(countryCode, 1) - asciiOffset + flagOffset
 		val flag = (String(Character.toChars(firstChar)) + String(Character.toChars(secondChar)))
-		countryListWithEmojis.add("$countryName (${locale.country} $flag")
+		countryListWithEmojis.add("$countryName (${locale.country}) $flag")
 	}
 	return countryListWithEmojis
 }
