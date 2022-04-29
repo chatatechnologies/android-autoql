6943661cea6f4c6f13fc024d924029200dec2ef7
Carlos Buruel <carlos@rinro.com.mx>
2022-04-29 13:37:57 -0500

    (04) custom topBar for image on left

--- app/src/main/java/chata/can/chata_ai_api/screens/MainManagePager.kt
+++ app/src/main/java/chata/can/chata_ai_api/screens/MainManagePager.kt
@@ -3,18 +3,22 @@
 package chata.can.chata_ai_api.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import chata.can.chata_ai.compose.ui.theme.ApiChataTheme
 import chata.can.chata_ai_api.R
 import chata.can.chata_ai_api.component.tabs.TabItemSealed
@@ -23,7 +27,6 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainManagerPager() {
@@ -35,18 +38,53 @@ fun MainManagerPager() {
 				TabManagePage(tabItems = tabInitialList, pagerState = pagerState)
 			},
 			topBar = {
-				val coroutineScope = rememberCoroutineScope()
+				//region tabRow
+				val blueAccentColor = colorResource(id = R.color.colorButton)
+				val blackColor = Color.Black
+				var selectedIndex by remember { mutableStateOf(0) }
+				val getTabColor: (Int) -> Color = remember(selectedIndex) {
+					{ index ->
+						if (selectedIndex == index) blueAccentColor else blackColor
+					}
+				}
 
-				IconWithTextManageTabLayout(
-					tabInitialList,
-					selectedIndex = pagerState.currentPage,
-					onPageSelected = { tabItem: TabItemSealed ->
-						coroutineScope.launch {
-							pagerState.animateScrollToPage(tabItem.index)
+				TabRow(
+					selectedTabIndex = selectedIndex,
+					backgroundColor = Color.White,
+					contentColor = blueAccentColor
+				) {
+					tabInitialList.forEachIndexed { index, tabItemSealed ->
+						Tab(selected = selectedIndex == index, onClick = { selectedIndex = index }) {
+							Row(verticalAlignment = Alignment.CenterVertically) {
+								Image(
+									painter = painterResource(id = tabItemSealed.imageResource),
+									colorFilter = ColorFilter.tint(getTabColor(index)),
+									contentDescription = ""
+								)
+								Text(
+									text = tabItemSealed.title,
+									modifier = Modifier.padding(12.dp),
+									style = TextStyle(color = getTabColor(index))
+								)
+							}
 						}
 					}
-				)
+				}
+				//endregion
 			}
+//			topBar = {
+//				val coroutineScope = rememberCoroutineScope()
+//
+//				IconWithTextManageTabLayout(
+//					tabInitialList,
+//					selectedIndex = pagerState.currentPage,
+//					onPageSelected = { tabItem: TabItemSealed ->
+//						coroutineScope.launch {
+//							pagerState.animateScrollToPage(tabItem.index)
+//						}
+//					}
+//				)
+//			}
 		)
 	}
 }
